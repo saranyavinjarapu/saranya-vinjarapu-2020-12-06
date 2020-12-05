e3cc425312f0a6ead62eeecd285505618cc41899
Saranya Vinjarapu <saranya.vinjarapu@gmail.com>
2020-12-05 22:45:37 +0530

    added search bar for searching jobs using keywords

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import './styles/main.css';
 import Header from "./components/Header";
+import AppBody from "./components/AppBody";
 import Footer from "./components/Footer";
-
-
 
 function App() {
   return (
     <div className="App"> 
       <div className="bg-gray-200">
         <Header/>  
-      
+        <AppBody/>      
         <Footer/>        
       </div>     
     </div>
---
+++ src/components/AppBody.js
@@ -0,0 +1,16 @@
+import React from "react";
+import SearchBar from './SearchBar';
+
+const AppBody = () => {
+    return (
+        <div className="container mx-auto pt-28 pb-28">
+          <div className = "mt-4 mx-4">
+            <SearchBar></SearchBar> 
+          </div>           
+        </div>        
+    );
+};
+
+export default AppBody;
+
+
