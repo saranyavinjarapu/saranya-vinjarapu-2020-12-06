75e4bb0127582437f6c0d452f712b28416019917
Saranya Vinjarapu <saranya.vinjarapu@gmail.com>
2020-12-05 21:05:00 +0530

    added header and footer for the web page as per the screenshot provided

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import './styles/main.css';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
+
 
 function App() {
   return (
-    <div className="App">
+    <div className="App"> 
+      <div className="bg-gray-200">
+        <Header/>  
       
+        <Footer/>        
+      </div>     
     </div>
   );
 }
